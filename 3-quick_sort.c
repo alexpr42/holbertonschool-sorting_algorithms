0c53658a04332914194dd1dec3fce9bec35c6b38
alexpr42 <8988@holbertonstudents.com>
2024-04-08 13:31:52 -0400

    fix to 3-quick_sort.c file in declaration area

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -3,8 +3,6 @@
 /**
  * quick_sort - Sorts an array of integers in
  * ascending order using Quick sort
- * quick_sort_recursive - declare low and high
- * lomuto_partition - use low and high
  * @array: Pointer to the array to be sorted
  * @size: Size of the array
  */
@@ -12,6 +10,14 @@ void quick_sort(int *array, size_t size)
 {
 	quick_sort_recursive(array, 0, size - 1, size);
 }
+
+/**
+ * quick_sort_recursive - Recursively sorts a partition of the array
+ * @array: Pointer to the array to be sorted
+ * @low: The lowest index of the partition
+ * @high: The highest index of the partition
+ * @size: Size of the original array
+ */
 
 void quick_sort_recursive(int *array, int low, int high, size_t size)
 {
@@ -24,6 +30,16 @@ void quick_sort_recursive(int *array, int low, int high, size_t size)
 		quick_sort_recursive(array, pivot + 1, high, size);
 	}
 }
+
+/**
+ * lomuto_partition - Partitions the array using Lomuto partition scheme
+ * @array: Pointer to the array to be partitioned
+ * @low: The lowest index of the partition
+ * @high: The highest index of the partition
+ * @size: Size of the original array
+ *
+ * Return: The index of the pivot element
+ */
 
 int lomuto_partition(int *array, int low, int high, size_t size)
 {
