4c27b08aa426e3fcb1f4824c6643823aa516fb6b
alexpr42 <8988@holbertonstudents.com>
2024-04-08 00:14:21 -0400

    fix variables

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,11 +7,13 @@
  */
 void insertion_sort_list(listint_t **list)
 {
+	listint_t *current;
+	listint_t *temp;
+
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	listint_t *current = (*list)->next;
-	listint_t *temp;
+	current = (*list)->next;
 
 	while (current != NULL)
 	{
